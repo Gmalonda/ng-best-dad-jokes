b40a7631d68567d2f5d99f27357a5c5659cbdeb4
Gedeon Malonda-Omande <gedeon.malonda-omande@diiage.org>
2021-12-13 15:43:01 +0100

    feat(partie 2): Routing

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,19 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 import { postsMock } from '../data/posts.mock';
 import { Post } from '../models/post.interface';
 import { PostService } from '../services/post.service';
+import { Pipe, PipeTransform } from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss'],
 })
+
 export class PostComponent implements OnInit {
   posts: Array<Post>;
   post?: Post;
-  constructor(private route: ActivatedRoute, private postService : PostService) {
+  
+
+  constructor(private route: ActivatedRoute, private postService : PostService, private router : Router) {
     this.posts = new Array<Post>();
+
   }
 
   ngOnInit(): void {
@@ -28,5 +34,7 @@ export class PostComponent implements OnInit {
       }
     );
 
+
+   
   }
 }
